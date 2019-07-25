552915711b8a58d82429ae2f21abaef04be38873
luojia65 <me@luojia.cc>
2019-07-25 20:45:08 +0800

    update

---
+++ implementors/core/cmp/trait.Eq.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["coruscant_world"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"enum\" href=\"coruscant_world/dimension/enum.Dimension.html\" title=\"enum coruscant_world::dimension::Dimension\">Dimension</a>",synthetic:false,types:["coruscant_world::dimension::Dimension"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/core/hash/trait.Hash.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["coruscant_world"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"enum\" href=\"coruscant_world/dimension/enum.Dimension.html\" title=\"enum coruscant_world::dimension::Dimension\">Dimension</a>",synthetic:false,types:["coruscant_world::dimension::Dimension"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
